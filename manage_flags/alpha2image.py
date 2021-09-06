b80e9e0f24103de33f9bf49d93576bf933ccd7bb
Seth Fischer <seth@fischer.nz>
2021-09-06 20:03:29 +1200

    fix: add dimensions to placeholder images
    Closes #1

--- manage_flags/alpha2image.py
+++ manage_flags/alpha2image.py
@@ -2,7 +2,7 @@ class Alpha2Image:
 
     template = """\
 <?xml version="1.0" encoding="UTF-8"?>
-<svg version="1.1" xmlns="http://www.w3.org/2000/svg">
+<svg version="1.1" width="960" height="480" viewBox="0 0 960 480" xmlns="http://www.w3.org/2000/svg">
   <rect width="960" height="480" fill="gray" stroke="#000"/>
   <text x="220" y="360" font-family="monospace" font-size="430">{alpha_2}</text>
 </svg>
