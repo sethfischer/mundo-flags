b15213eaa796295f1962d29f9d59e5c83f9c78e3
Seth Fischer <seth@fischer.nz>
2021-09-07 21:41:18 +1200

    build: templating engine for placeholder images

--- manage_flags/alpha2image.py
+++ manage_flags/alpha2image.py
@@ -1,15 +1,15 @@
+from jinja2 import Environment, PackageLoader, select_autoescape
+
+
 class Alpha2Image:
-
-    template = """\
-<?xml version="1.0" encoding="UTF-8"?>
-<svg version="1.1" width="960" height="480" viewBox="0 0 960 480" xmlns="http://www.w3.org/2000/svg">
-  <rect width="960" height="480" fill="gray" stroke="#000"/>
-  <text x="220" y="360" font-family="monospace" font-size="430">{alpha_2}</text>
-</svg>
-"""
-
     def __init__(self, alpha_2: str):
         self.alpha_2 = alpha_2
 
     def get(self) -> str:
-        return self.template.format(alpha_2=self.alpha_2)
+        env = Environment(
+            loader=PackageLoader("manage_flags"), autoescape=select_autoescape()
+        )
+
+        template = env.get_template("alpha2image.svg")
+
+        return template.render(alpha_2=self.alpha_2)
