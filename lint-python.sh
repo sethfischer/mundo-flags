e3dedd396352c6f1aa6549ee835055421a954201
Seth Fischer <seth@fischer.nz>
2021-09-21 22:03:58 +1200

    ci: pydocstyle

--- lint-python.sh
+++ lint-python.sh
@@ -7,3 +7,4 @@ directory="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 git ls-files -z -- "${directory}/**.py" | xargs --no-run-if-empty --null black --check
 git ls-files -z -- "${directory}/**.py" | xargs --no-run-if-empty --null flake8
 git ls-files -z -- "${directory}/**.py" | xargs --no-run-if-empty --null isort --profile black --check-only
+git ls-files -z -- "${directory}/**.py" ':!:tests/*' | xargs --no-run-if-empty --null pydocstyle --convention=pep257
