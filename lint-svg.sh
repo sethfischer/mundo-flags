d474ad4194b3a14aad7019a611829b7652336354
Seth Fischer <seth@fischer.nz>
2021-09-28 19:51:58 +1300

    style: one scour option per line

--- lint-svg.sh
+++ lint-svg.sh
@@ -9,7 +9,15 @@ directory="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 while IFS= read -r -d $'\0' filename; do
     if [ -f "$filename" ]; then
         echo "Check formatting of ${filename}"
-        diff "$filename" <(scour --quiet -i "$filename" --disable-simplify-colors --disable-style-to-xml --indent=space --keep-unreferenced-defs --nindent=2 --protect-ids-noninkscape --enable-comment-stripping --strip-xml-space) 1>&2
+        diff "$filename" <(scour --quiet -i "$filename" \
+            --nindent=2 \
+            --indent=space \
+            --keep-unreferenced-defs \
+            --protect-ids-noninkscape \
+            --disable-simplify-colors \
+            --enable-comment-stripping \
+            --strip-xml-space \
+            --disable-style-to-xml) 1>&2
         # shellcheck disable=SC2181
         if [ $? -ne 0 ]; then
             exit_code=1
